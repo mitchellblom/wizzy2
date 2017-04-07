67cd7bd913151c62d757393e197f21bb5b63383d
Mitchell Blom <mitchellblom@gmail.com>
2017-04-06 20:24:37 -0500

    clicks setting input equal to whatever clicked inside container

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
-// console.log("js works");
-
-// $("#userInputTextBox").
-
 var data;
 
 $.ajax({
 	url: ["people.json"]
 }).done( function (data) {
-	console.log(data);
 	doSomething(data);
 }).fail( function (error) {
 	console.log("error");
-}).always( function (data){
-	// example goes here
 });
 
 function doSomething (data) {
-	console.log(data.people.length);
 	for (var i = 0; i < data.people.length; i++) {
 		var card = "";
-			card += `<div class="person__container">`
-			card +=	`<p>${data.people[i].title} ${data.people[i].name}</p>`
-			card +=	`<img class="bioPic" src="${data.people[i].image}" alt="person pic">`
-			card +=	`<p>${data.people[i].bio}</p>`
-			card +=	`</div>`
-		$("#cardContainer").append(card)
-		// card += `${data.name}`
+			card += `<div class="person__container">`;
+			card +=	`<p>${data.people[i].title} ${data.people[i].name}</p>`;
+			card +=	`<img class="bioPic" src="${data.people[i].image}" alt="person pic">`;
+			card +=	`<p class="bioDescrip" id="bioDescrip[i]">${data.people[i].bio}</p>`;
+			card +=	`</div>`;
+		$("#cardContainer").append(card);
 	}
 }
+
+$("#cardContainer").click(function(data) {
+	if ($(".bioDescrip")) {
+	$("#userInputTextBox").focus();
+	// console.log($("data.target").html());
+	// console.log(data.target.innerHTML);
+	$("#userInputTextBox").val(data.target.innerHTML);
+	};
+ });
