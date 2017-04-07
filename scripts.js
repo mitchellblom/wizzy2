4921ee111d7b6df7a0546572d01cad737db7790d
Mitchell Blom <mitchellblom@gmail.com>
2017-04-06 22:20:03 -0500

    working except for when clicking between two descrips

--- scripts.js
+++ scripts.js
@@ -1,44 +1,41 @@
 var data;
+var userInputText = $("#userInputTextBox");
 
 $.ajax({
-	url: ["people.json"]
-}).done( function (data) {
-	doSomething(data);
-}).fail( function (error) {
-	console.log("error");
+    url: ["people.json"]
+}).done(function(data) {
+    createCards(data);
+}).fail(function(error) {
+    console.log("error");
 });
 
-function doSomething (data) {
-	for (var i = 0; i < data.people.length; i++) {
-		var card = "";
-			card += `<div class="person__container">`;
-			card +=	`<p>${data.people[i].title} ${data.people[i].name}</p>`;
-			card +=	`<img class="bioPic" src="${data.people[i].image}" alt="person pic">`;
-			card +=	`<p class="bioDescrip" id="bioDescrip[i]">${data.people[i].bio}</p>`;
-			card +=	`</div>`;
-			var descripI = data.people[i];
-
-		$("#cardContainer").append(card);
-	}
+function createCards(data) {
+    for (var i = 0; i < data.people.length; i++) {
+        var card = "";
+        card += `<div class="person_container">`;
+        card += `<p>${data.people[i].title} ${data.people[i].name}</p>`;
+        card += `<img class="bioPic" src="${data.people[i].image}" alt="person pic">`;
+        card += `<p class="bioDescrip" id="bioDescrip[i]">${data.people[i].bio}</p>`;
+        card += `</div>`;
+        $("#cardContainer").append(card);
+    }
 }
 
 $("#cardContainer").click(function(data) {
-	var clickedBio = data.target;
-	if (clickedBio.className === "bioDescrip") {
-	$("#userInputTextBox").focus();
-	$("#userInputTextBox").val(clickedBio.innerHTML);
-	} else {
-		$("#userInputTextBox").val("");
-		clickedBio = "";
-	}
-	$("#userInputTextBox").keypress(function() {
-			clickedBio.innerHTML = $("#userInputTextBox").val();
-            // $("#userInputTextBox").keydown() = function() {
-            // 	console.log("keydown");
-                // if (window.event.keyCode === 13) {
-                //     clickedBio = "";
-                //     userInputText.value = "";
-                // }
-            // }
+    var clickedBio = data.target;
+    if (clickedBio.className === "bioDescrip") {
+        userInputText.focus();
+        userInputText.val(clickedBio.innerHTML);
+        userInputText.keydown(function() {
+            clickedBio.innerHTML = userInputText.val();
+            console.log("keypress");
+            if (window.event.keyCode === 13 && userInputText.val() !== "" && clickedBio !== "") {
+                userInputText.val("");
+                clickedBio = "";
+            }
         });
- });
+    } else {
+        userInputText.val("");
+        clickedBio = "";
+    }
+});
